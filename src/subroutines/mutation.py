35584034e00f7805c44f7f603e4bbbef28ebb057
RubenIbarrondo <rubenibarrondo@gmail.com>
2024-06-06 10:42:42 +0200

    Add mutation and tests, not bugfixed

--- src/subroutines/mutation.py
+++ src/subroutines/mutation.py
@@ -1,6 +1,9 @@
 import numpy as np
 from abc import ABC, abstractmethod
 
+from pyqch.channel_families import probabilistic_unitaries
+from pyqch import state_transformations
+from pyqch import random_generators
 
 class MutationSubroutine(ABC):
 
@@ -10,8 +13,69 @@ class MutationSubroutine(ABC):
 
 class RandomPauli(MutationSubroutine):
 
-    def __init__(self, **kwargs):
-        pass
+    def __init__(self, chromosome_size: int, population_size: int, mutation_probability: float, random_state: int | np.random.Generator = None, **kwargs):
+        if random_state is not None:
+            self.random_state = np.random.default_rng(random_state)
+        else:
+            # As the methods in random use the default random generator
+            # we access it via the __self__ attribute
+            self.random_state = np.random.random.__self__
 
-    def mutate(self, state: np.ndarray) -> np.ndarray:
-        return state
+        self.chromosome_size = chromosome_size
+        self.population_size = population_size
+        self.mutation_probability = mutation_probability
+        self.system_shape = (2,) * (self.chromosome_size * self.population_size)
+
+        # Denfine the Pauli matrices
+        paulix = np.zeros((2, 2))
+        paulix[0,1] = 1
+        paulix[1,0] = 1
+
+        pauliy = np.zeros((2, 2), dtype=complex)
+        pauliy[0,1] = -1j
+        pauliy[1,0] = 1j
+
+        pauliz = np.diag([1,-1])
+        
+        # Define the channels for applying the Pauli operators
+        self.x = np.kron(paulix, paulix)
+        self.y = np.kron(pauliy, pauliy.T)
+        self.z = np.kron(pauliz, pauliz)
+        self.unitaries_aschannel_array = np.array([np.identity(4), self.x, self.y, self.z])
+
+        # Define the channel for applying a random Pauli operator
+        self.probability_array = np.array([1-self.mutation_probability,
+                                      self.mutation_probability/3,
+                                      self.mutation_probability/3,
+                                      self.mutation_probability/3])
+        self.unitaries_array = np.array([np.identity(2), paulix, pauliy, pauliz])
+        self.local_mutation_mat = probabilistic_unitaries(2,
+                                                          self.probability_array,
+                                                          self.unitaries_array)
+                                                          
+
+    def mutate(self, state: np.ndarray, random_select: bool = False) -> np.ndarray:
+        if random_select:
+            mutation_pattern = self.random_state.choice(4,
+                                                        self.chromosome_size * self.population_size,
+                                                        p = self.probability_array)
+            state_mut = state.copy()
+            for chromosome, unitary_index in enumerate(mutation_pattern):
+                if unitary_index > 0:
+                    paulit = self.unitaries_aschannel_array[unitary_index]
+                    state_mut = state_transformations.local_channel(state_mut,
+                                                                    self.system_shape,
+                                                                    chromosome,
+                                                                    paulit)
+        else:
+            state_mut = state.copy()
+            print(f'Purity before mutating: {np.trace(state_mut @ state_mut).real:.3f} expected {1}')
+
+            for chromosome in range(self.chromosome_size * self.population_size):
+                state_mut = state_transformations.local_channel(state_mut,
+                                                                self.system_shape,
+                                                                chromosome,
+                                                                self.local_mutation_mat)
+                print(np.diag(state_mut))
+                print(f'Purity after mutating chromosome {chromosome}: {np.trace(state_mut @ state_mut).real:.3f} expected <={1/(chromosome+2):.3f}')
+        return state_mut
---
+++ tests/test_subroutines_mutation.py
@@ -0,0 +1,142 @@
+import unittest
+
+import numpy as np
+from pyqch import predicates
+from pyqch import channel_operations
+from pyqch import state_transformations
+from pyqch import channel_families
+from pyqch import random_generators
+from src.subroutines import mutation
+
+class TestSubroutinesMutation_RandomPauli(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.chromosome_size = 2
+        self.population_size = 2
+        self.mutation_probability = .1
+
+        self.system_shape = (2,) * (self.chromosome_size * self.population_size)
+        self.system_dim = 2 ** (self.chromosome_size * self.population_size)
+        self.random_state = 24597856786290
+
+        self.rpauli = mutation.RandomPauli(self.chromosome_size, self.population_size, self.mutation_probability)
+
+        self.pop_state = np.zeros((self.system_dim, self.system_dim))
+        self.pop_state[0,0] = 1
+
+        self.arbpsi = random_generators.state(self.system_dim, 1, self.random_state)
+    
+    def test_shapes(self):
+        for u_aschannel in self.rpauli.unitaries_aschannel_array:
+            self.assertEqual(u_aschannel.shape, (4,4))
+        for u in self.rpauli.unitaries_array:
+            self.assertEqual(u.shape, (2,2))
+        
+        pop_state2 = self.rpauli.mutate(self.pop_state)
+        self.assertEqual(pop_state2.shape, self.pop_state.shape)
+
+        pop_state3 = self.rpauli.mutate(self.pop_state, random_select=True)
+        self.assertEqual(pop_state3.shape, self.pop_state.shape)
+
+    def test_check_paulis(self):
+
+        paulis = [np.array([[1,0],[0,1]]),
+                  np.array([[0,1],[1,0]]),
+                  np.array([[0,-1j],[1j,0]]),
+                  np.array([[1,0],[0,-1]])]
+
+        for i, u in enumerate(self.rpauli.unitaries_array):
+            np.testing.assert_array_almost_equal(u, paulis[i])
+
+    def test_is_channel(self):
+        for u_aschannel in self.rpauli.unitaries_aschannel_array:
+            self.assertTrue(predicates.is_channel(u_aschannel))
+        for u in self.rpauli.unitaries_array:
+            np.testing.assert_array_almost_equal(u @ u.T.conj(), np.identity(2))
+
+    def test_unitality(self):
+        maxmix = np.identity(self.system_dim)
+
+        maxmix2 = self.rpauli.mutate(maxmix)
+        maxmix3 = self.rpauli.mutate(maxmix, random_select=True)
+
+        np.testing.assert_array_almost_equal(maxmix2, maxmix)
+        np.testing.assert_array_almost_equal(maxmix3, maxmix)
+        
+    def test_probzero_isidentity(self):
+        zeromut = mutation.RandomPauli(self.chromosome_size, self.population_size, 0.0)
+        arbpsi2 = zeromut.mutate(self.arbpsi)
+        np.testing.assert_array_almost_equal(arbpsi2, self.arbpsi)
+
+        arbpsi3 = zeromut.mutate(self.arbpsi, random_select=True)
+        np.testing.assert_array_almost_equal(arbpsi3, self.arbpsi)
+        
+    def test_prob34_ismaximallymixed(self):
+        ppauli_from_depol = 3/4
+        self.arbpsi = self.pop_state
+        onemut = mutation.RandomPauli(self.chromosome_size, self.population_size, ppauli_from_depol)
+        arbpsi2 = onemut.mutate(self.arbpsi)
+        print(np.trace(arbpsi2 @ arbpsi2), 1/self.system_dim)
+        print(onemut.local_mutation_mat)
+        np.testing.assert_array_almost_equal(arbpsi2,
+                                             np.identity(self.system_dim) / self.system_dim)
+        
+        # arbpsi2 = onemut.mutate(self.arbpsi, random_select=True)
+            
+    def test_aschannel_onpurestate(self):
+        # Should be a local depolarizer
+        arbpsi2 = self.rpauli.mutate(self.arbpsi)
+
+        pdepol = 4 / 3  * self.mutation_probability
+        tdepol = channel_families.depolarizing(2, pdepol)
+
+        arbpsi2_ref = self.arbpsi.copy()
+        for chromosome in range(self.chromosome_size * self.population_size):
+            arbpsi2_ref = state_transformations.local_channel(arbpsi2_ref,
+                                                              self.system_shape,
+                                                              chromosome,
+                                                              tdepol)
+        np.testing.assert_array_almost_equal(arbpsi2, arbpsi2_ref)
+
+    def test_asrandom_onpurestate(self):
+        # The input and output states should have same purity
+        arbpsi2 = self.rpauli.mutate(self.arbpsi, random_select=True)
+        self.assertAlmostEqual(np.trace(arbpsi2 @ arbpsi2), np.trace(self.arbpsi @ self.arbpsi))
+
+        # in local, should be one of some options for the random select one
+        psi_local = state_transformations.partial_trace(self.arbpsi,
+                                                        self.system_shape,
+                                                        range(1, len(self.system_shape)))
+        psi2_local = state_transformations.partial_trace(self.arbpsi,
+                                                         self.system_shape,
+                                                         range(1, len(self.system_shape)))
+        equal_to_some_pauli_rot = False
+        for pauli in self.rpauli.unitaries_array:
+            if np.allclose(psi2_local, pauli @ psi_local @ pauli.T.conj()):
+                equal_to_some_pauli_rot = True
+                break
+        self.assertTrue(equal_to_some_pauli_rot)
+
+if __name__ == "__main__":
+
+    # Add this test in pyqch
+    from pyqch import channel_families
+    import numpy as np
+
+    paulis = np.array([np.array([[1,0],[0,1]]),
+                       np.array([[0,1],[1,0]]),
+                       np.array([[0,-1j],[1j,0]]),
+                       np.array([[1,0],[0,-1]])])
+    p = 3/4-.0123
+    prob_array = np.zeros(paulis.shape[0])
+    prob_array[0] = 1 - p
+    prob_array[1:] = p / 3
+
+    pdepol = 4 / 3  * p  # 2 ** (2*n) / (2 ** (2*n) - 1) * p
+
+    trandu = channel_families.probabilistic_unitaries(2,
+                                                      prob_array,
+                                                      paulis)
+    tdepol = channel_families.depolarizing(2, pdepol)
+
+    np.testing.assert_array_almost_equal(trandu, tdepol)
