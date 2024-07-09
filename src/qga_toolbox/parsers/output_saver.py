84e5a2f30d1e820c7d6ab69b45b3be2a7cd2609d
RubenIbarrondo <rubenibarrondo@gmail.com>
2024-07-09 20:34:35 +0200

    Add notebooks.

--- src/qga_toolbox/parsers/output_saver.py
+++ src/qga_toolbox/parsers/output_saver.py
@@ -4,9 +4,15 @@ import numpy as np
 
 def save_outputs(outputs, output_directory_path: str, input_id: str, problem_instance_id: int, state_instance_id: int):
     
-    # check colisions in outputs...
+    # check collisions in outputs...
     final_state, tracked_features = outputs
     tracked_features['state'] = final_state
+
+    if not os.path.exists(output_directory_path):
+        os.mkdir(output_directory_path)
+    if not os.path.exists(os.path.join(output_directory_path, input_id)):
+        os.mkdir(os.path.join(output_directory_path, input_id))
+
     try:        
         for feature_name, feature_arr in tracked_features.items():
             file_name = f"problem_{problem_instance_id}_state_{state_instance_id}_{feature_name}"
@@ -15,8 +21,6 @@ def save_outputs(outputs, output_directory_path: str, input_id: str, problem_ins
                     np.array(feature_arr))
             
     except FileNotFoundError as fnfe:
-        if not os.path.exists(output_directory_path):
-            os.mkdir(output_directory_path)
-        if not os.path.exists(os.path.join(output_directory_path, input_id)):
-            os.mkdir(os.path.join(output_directory_path, input_id))
+        raise fnfe
+        
 
