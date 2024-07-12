92221afbf3cc1a977cece1d6a8413ecf9e96517b
RubenIbarrondo <rubenibarrondo@gmail.com>
2024-07-12 10:45:45 +0200

    Add BCQO cloning subroutine.

--- src/qga_toolbox/subroutines/cloning.py
+++ src/qga_toolbox/subroutines/cloning.py
@@ -13,25 +13,13 @@ class CloningSubroutine(AbstractSubroutine):
     def clone(self, state: np.ndarray, single_individual: bool = False) -> np.ndarray:
         return state
 
-class UQCM(CloningSubroutine):
+class PairWiseClone(CloningSubroutine):
 
-    def __init__(self, chromosome_size: int, population_size: int, **kwargs):
+    def __init__(self, chromosome_size: int, population_size: int, clone_mat: np.ndarray, **kwargs):
         super().__init__(chromosome_size, population_size, **kwargs)
         self.n = int(population_size)
 
-        # Build the projector onto the symmetric subspace of 2 individuals
-        id2 = np.identity(self.dim ** 2)
-        swap = np.einsum('ijkl->jikl', id2.reshape((self.dim,) * 4)).reshape((self.dim ** 2, self.dim ** 2))
-        sym_proj = .5 * (swap + id2)
-        sym_proj = np.kron(sym_proj, sym_proj)
-
-        # Build the extending with identity operation
-        states = (np.identity(self.dim) / self.dim).reshape((1, self.dim, self.dim))
-        id_extend = co.tensor([np.identity(self.dim**2),
-                               cf.initializer(self.dim, states)])
-        
-        # Build 1 to 2 cloning matrix
-        self.clone_mat = 2 * self.dim / (self.dim + 1) * sym_proj @ id_extend
+        self.clone_mat = clone_mat
 
     def clone(self, state: np.ndarray, single_individual: bool = False) -> np.ndarray:
         if single_individual:
@@ -52,6 +40,54 @@ class UQCM(CloningSubroutine):
             state = st.subsystem_permutation(state, (self.dim,) * n_current, permutation=tuple(perm))
 
         return state
+
+class UQCM(PairWiseClone):
+
+    def __init__(self, chromosome_size: int, population_size: int, **kwargs):
+        self.dim = 2 ** chromosome_size
+
+        # Build the projector onto the symmetric subspace of 2 individuals
+        id2 = np.identity(self.dim ** 2)
+        swap = np.einsum('ijkl->jikl', id2.reshape((self.dim,) * 4)).reshape((self.dim ** 2, self.dim ** 2))
+        sym_proj = .5 * (swap + id2)
+        sym_proj = np.kron(sym_proj, sym_proj)
+
+        # Build the extending with identity operation
+        states = (np.identity(self.dim) / self.dim).reshape((1, self.dim, self.dim))
+        id_extend = co.tensor([np.identity(self.dim**2),
+                               cf.initializer(self.dim, states)])
+        
+        # Build 1 to 2 cloning matrix
+        clone_mat = 2 * self.dim / (self.dim + 1) * sym_proj @ id_extend
+
+        super().__init__(chromosome_size, population_size, clone_mat=clone_mat, **kwargs)
+
+class BCQO(PairWiseClone):
+
+    def __init__(self, chromosome_size: int, population_size: int, cloning_basis: int | np.ndarray, **kwargs):
+        
+        self.dim = 2 ** chromosome_size
+
+        self.cloning_basis = cloning_basis
+
+        # Build 1 to 2 cloning matrix in computational basis
+        clone_isometry_computational = np.zeros((self.dim,) * (2+1))
+        for i in range(self.dim):
+            clone_isometry_computational[i,i,i]=1
+        clone_isometry_computational = clone_isometry_computational.reshape((self.dim**2, self.dim))
+        
+        if isinstance(self.cloning_basis, int) and self.cloning_basis == 1:
+            clone_isometry = clone_isometry_computational
+        elif isinstance(self.cloning_basis, np.ndarray):
+            clone_isometry = np.dot(np.kron(self.cloning_basis.T.conj(), self.cloning_basis.T.conj()),
+                                    np.dot(clone_isometry_computational,
+                                           self.cloning_basis))
+        else:
+            raise ValueError(f"Unexpected value for cloning_basis, expected 1 or np.ndarray but obtained:\n{cloning_basis}.")
+        
+        clone_mat = np.kron(clone_isometry, clone_isometry.conj())
+
+        super().__init__(chromosome_size, population_size, clone_mat=clone_mat, **kwargs)
     
 
 if __name__ == "__main__":
--- tests/test_subroutines_cloning.py
+++ tests/test_subroutines_cloning.py
@@ -4,7 +4,7 @@ import numpy as np
 from pyqch import predicates
 from pyqch import channel_operations
 from pyqch import state_transformations
-from pyqch import state_families
+from pyqch import random_generators
 from qga_toolbox.subroutines import cloning
 
 class TestSubroutinesCloning_UQCM(unittest.TestCase):
@@ -95,3 +95,107 @@ class TestSubroutinesCloning_UQCM(unittest.TestCase):
 
         with self.assertRaises(ValueError):
             self.uqcm.clone(too_big_pop_state)
+
+
+class TestSubroutinesCloning_BCQO(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.chromosome_size = 2
+        self.population_size = 4
+        self.cloning_basis = 1
+
+        self.bcqo = cloning.BCQO(self.chromosome_size, self.population_size, self.cloning_basis)
+
+        self.psi = random_generators.state(self.bcqo.dim, rank=1, random_state=123)
+
+        self.pop_state = np.zeros((self.bcqo.dim ** (self.population_size // 2),) * 2)
+        self.pop_state[0,0] = 1
+
+
+    def test_shapes(self):
+        self.assertEqual(self.bcqo.clone_mat.shape, (self.bcqo.dim ** 4, self.bcqo.dim ** 2))
+            
+    def test_is_channel(self):
+        self.assertTrue(predicates.is_channel(self.bcqo.clone_mat))
+
+    def test_symmetry(self):
+        choi = channel_operations.choi_state(self.bcqo.clone_mat)
+        choi_swap = state_transformations.subsystem_permutation(choi,
+                                                                (self.bcqo.dim,) * 3,
+                                                                (1, 0, 2))
+        np.testing.assert_array_almost_equal(choi, choi_swap)
+
+    def test_cloning_pure_states(self):
+        psi_clone = self.bcqo.clone(self.psi, single_individual=True)
+
+        psi_ref = np.zeros((self.bcqo.dim,)*4, dtype=complex)
+        for i in range(self.bcqo.dim):
+            for j in range(self.bcqo.dim):
+                psi_ref[i,i,j,j] = self.psi[i,j]
+        psi_ref = psi_ref.reshape(((self.bcqo.dim**2,)*2))
+
+        np.testing.assert_array_almost_equal(psi_clone, psi_ref)
+    
+    def test_cloning_pure_states_random_basis(self):
+        from scipy.stats import unitary_group
+
+        u = unitary_group.rvs(2 ** self.chromosome_size, random_state=np.random.default_rng(123))
+        bcqo_u = cloning.BCQO(self.chromosome_size, self.population_size,
+                              cloning_basis=u)
+        
+        psi_clone = bcqo_u.clone(self.psi, single_individual=True)
+
+        psi_u = u @ self.psi @ u.T.conj()
+        psi_ref = np.zeros((self.bcqo.dim,)*4, dtype=complex)
+        for i in range(bcqo_u.dim):
+            for j in range(bcqo_u.dim):
+                psi_ref[i,i,j,j] = psi_u[i,j]
+        psi_ref = np.kron(u.T.conj(), u.T.conj()) @ psi_ref.reshape(((bcqo_u.dim**2,)*2)) @ np.kron(u, u)
+
+        np.testing.assert_array_almost_equal(psi_clone, psi_ref)
+    
+    def test_maximally_mixed(self):
+        d = self.bcqo.dim
+        mixed = np.identity(d)
+        mixed_clone = self.bcqo.clone(mixed, single_individual=True)
+
+        mixed_ref = np.zeros((self.bcqo.dim,)*4, dtype=complex)
+        for i in range(self.bcqo.dim):
+            for j in range(self.bcqo.dim):
+                mixed_ref[i,i,j,j] = mixed[i,j]
+        mixed_ref = mixed_ref.reshape((self.bcqo.dim**2,)*2)
+
+        np.testing.assert_array_almost_equal(mixed_clone, mixed_ref)
+
+    def test_poplevel(self):
+
+        pop_state_clone = self.bcqo.clone(self.pop_state)
+
+
+        self.assertEqual(pop_state_clone.shape, (self.bcqo.dim ** self.population_size, self.bcqo.dim ** self.population_size))
+
+        self.assertTrue(predicates.is_density_matrix(pop_state_clone))
+
+        # Testing symmetry
+        pop_state_clone_swap = state_transformations.subsystem_permutation(pop_state_clone,
+                                                                           (self.bcqo.dim ** (self.population_size // 2),) * 2,
+                                                                           (1,0))
+        
+        np.testing.assert_array_almost_equal(pop_state_clone, pop_state_clone_swap)
+
+        # Testing that the reduced state of the first didn't change
+        rho1 = state_transformations.partial_trace(pop_state_clone,
+                                                    (self.bcqo.dim,) * self.population_size,
+                                                    list(range(1,self.population_size)))
+        rho1_ref = state_transformations.partial_trace(self.pop_state,
+                                                    (self.bcqo.dim,) * (self.population_size//2),
+                                                    list(range(1,self.population_size//2)))
+        np.testing.assert_array_almost_equal(rho1, rho1_ref)
+    
+
+    def test_error_if_state_too_big(self):
+        too_big_pop_state = np.zeros((self.bcqo.dim ** (self.population_size),) * 2)
+        too_big_pop_state[0,0] = 1
+
+        with self.assertRaises(ValueError):
+            self.bcqo.clone(too_big_pop_state)
